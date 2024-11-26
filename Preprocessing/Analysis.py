dbd4fe71f74c863212da2e7cc462d569f47cbe84
Shiva Heydari <shivaheydari.es@gmail.com>
2024-11-26 13:34:22 -0700

    EGG

--- Preprocessing/Analysis.py
+++ Preprocessing/Analysis.py
@@ -2,21 +2,24 @@
 
 from pymongo import MongoClient
 
-# client = MongoClient("mongodb://localhost:27017/")
-# db = client["MIMIC"]
-# list_collection = db.list_collection_names()
-# admission = db["Admission"]
-# print(admission.count_documents({}))
-# noteevents = db["Noteevents"]
-# print(noteevents.count_documents({}))
+client = MongoClient("mongodb://localhost:27017/")
+db = client["MIMIC"]
+list_collection = db.list_collection_names()
+admission = db["Admission"]
+print(admission.count_documents({}))
+noteevents = db["Noteevents"]
+print(noteevents.count_documents({}))
 
 
 #find the number of patients
 '''
 
-hospital readmisiions = unplanned visits within 30 days from earleier state. #admission_TYPE = "EMERGENCY" or "URGENT"
+hospital readmisiions = unplanned visits within 30 days from earleier state. #admission_TYPE = "EMERGENCY" or "URGENT"Exluce NEWBORNS, death, ...
 '''
 
-#hospital readmisiions = unplanned visits within 30 days from earleier state. #admission_TYPE = "EMERGENCY" or "URGENT"
+'''
 
-# remove newbors, transfers, deaths, and planned readmission. i
+remove : new newbors, transfers, deaths, and planned readmission: discharge_location: HOME
+'''
+
+
