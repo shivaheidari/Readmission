a229388c224d80f217360717b611b789ef10009c
Shiva Heydari <shivaheydari.es@gmail.com>
2024-12-13 13:16:24 -0700

    plots

--- Preprocessing/prepare.py
+++ Preprocessing/prepare.py
@@ -9,3 +9,10 @@ print(admission.count_documents({}))
 noteevents = db["Noteevents"]
 print(noteevents.count_documents({}))
 
+#from patients-admissions collection find patients with readmissions and transfer the data into another collection like patients-readmissions
+
+
+
+# from the collection patients-readmissions create a collection like patient; readmission; [notes] * notes comes from NOTEEVENTS refine categories 
+
+# create collection like patiet, concatenated note, label = 1
