ab0f62ea68e8d3b0e22d0a8d247a6962f3714f20
Shiva Heydari <shivaheydari.es@gmail.com>
2025-01-28 11:21:08 -0700

    newmodel

--- Deploying/app/model/load_model.py
+++ Deploying/app/model/load_model.py
@@ -1,11 +1,24 @@
 import torch
-from transformers import AutoModelForAudioClassification, AutoTokenizer
+from transformers import AutoModelForAudioClassification,BertTokenizer, AutoTokenizer
 import os
+import pickle
 
-MODEL_DIR = "../00_34_bert_custom"
-if os.path.exists(MODEL_DIR):
-    print("yes")
 
-# def load_model():
-#     tokenizer = Atu
-#     return model, tokenizer
+MODEL_DIR = "Readmission/Deploying/app/00_34_bert_custom/data.pkl"
+
+def load_model():
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    pass
+
+
+model = torch.load(MODEL_DIR)
+model.eval()
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+text = "this is a test"
+inputs = tokenizer(text, return_tensor="pt", padding=True, truncatin=True)
+
+with torch.no_grad():
+    outputs = model(**inputs)
+    predictions = torch.softmax(outputs.logits, dim=-1)
+
+print("predictions", predictions)
