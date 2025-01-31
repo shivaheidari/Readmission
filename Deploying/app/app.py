6274120412b07c41149bf638360d6867db5b5f44
Shiva Heydari <shivaheydari.es@gmail.com>
2025-01-31 13:27:18 -0700

    index

--- Deploying/app/app.py
+++ Deploying/app/app.py
@@ -15,7 +15,7 @@ def home():
 
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["POST", "GET"])
 def predict_endpoint():
     model_path = "Readmission/Deploying/app/00_66_bert_custom_dict"
     model = load_model(model_path)
